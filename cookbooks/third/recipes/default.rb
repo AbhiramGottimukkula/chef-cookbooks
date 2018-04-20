a40e0b695703512dfd4e639b2cb9619af3d80548
root <root@ip-172-31-11-13.ap-south-1.compute.internal>
2018-04-20 07:23:32 +0000

    third solution updated

--- cookbooks/third/recipes/default.rb
+++ cookbooks/third/recipes/default.rb
@@ -3,15 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-filestatus = File.exist?('/etc/passwd')
-log "#{filestatus}"
-if filestatus
-	log 'inside file exists'
-	file '/home/ubuntu/result.html' do
-		content '<html><h1>Working successfully!</h1></html>'
-		action :create
-	end
-	log 'file creating done'
-else
-	log 'file does not exist'
+file '/home/ubuntu/result.html' do
+  mode '0755'
+  content '<html><h1>Working fine!</h1></html>'
+  action :create
+  only_if 'test -f /etc/passwd'
 end
